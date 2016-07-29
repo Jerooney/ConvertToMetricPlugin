e3de7e3c832e78eb5ea80a70e062a9fadd1414b9
Jerooney <j.stevens@ultimaker.com>
2016-07-29 11:31:12 +0200

    Update ConvertToMetricPlugin.py

--- ConvertToMetricPlugin.py
+++ ConvertToMetricPlugin.py
@@ -8,7 +8,7 @@ i18n_catalog = i18nCatalog("ConvertToMetricPlugin")
 class ConvertToMetricPlugin(Extension):
     def __init__(self):
         super().__init__()
-        self.addMenuItem(i18n_catalog.i18n("Convert selected model"), self.convertToMetric)
+        self.addMenuItem(i18n_catalog.i18n("Convert Selected Model"), self.convertToMetric)
 
     def convertToMetric(self):
         selected_nodes = Selection.getAllSelectedObjects()
