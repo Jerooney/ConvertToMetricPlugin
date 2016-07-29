7a713c6db16f6a1d89779611cc17490a97c543e1
Jerooney <j.stevens@ultimaker.com>
2016-07-29 11:28:38 +0200

    Update and rename MetricScale.py to ConvertToMetricPlugin.py

--- ConvertToMetricPlugin.py
+++ ConvertToMetricPlugin.py
@@ -3,16 +3,14 @@ from UM.Scene.Selection import Selection
 from UM.Math.Vector import Vector
 
 from UM.i18n import i18nCatalog
-i18n_catalog = i18nCatalog("MetricScalePlugin")
+i18n_catalog = i18nCatalog("ConvertToMetricPlugin")
 
-class MetricScale(Extension):
+class ConvertToMetricPlugin(Extension):
     def __init__(self):
         super().__init__()
-        self.addMenuItem(i18n_catalog.i18n("Convert to metric"), self.convertToMetric)
+        self.addMenuItem(i18n_catalog.i18n("Convert selected model"), self.convertToMetric)
 
     def convertToMetric(self):
         selected_nodes = Selection.getAllSelectedObjects()
         for node in selected_nodes:
             node.scale(Vector(25.4,25.4,25.4))
-
-
